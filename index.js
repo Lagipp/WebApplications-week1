12d9e868d9f34b400370472ca7663090217e6ca2
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2021-09-13 18:17:32 +0300

    added barebones of .js contents

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", function () {
+  const bigText = document.getElementById("bug-text");
+
+  bigText.innerHTML = bigText.innerHTML + 
+});
+
+*/
