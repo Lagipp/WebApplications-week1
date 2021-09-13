6c2cc9f5bdfa85a3b15597a030d6c92f7d5fd79a
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2021-09-13 20:54:21 +0300

    idk trying again tmr

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const addButton = document.getElementById("add-comment");
 
+/*
 addButton.addEventListener("click", function () {
   const unorderedList = document.getElementById("unordered-list");
 
@@ -13,3 +14,26 @@ addButton.addEventListener("click", function () {
 
   unorderedList.appendChild(newListItem);
 });
+*/
+
+addButton.addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+
+  let newText = document.createElement("p");
+  newText.innerHTML = "test";
+
+  textArea.appendChild(newText);
+});
+
+/*
+const addButton = document.getElementById("add-comment");
+
+addButton.addEventListener("click", function () {
+  const unorderedList = document.getElementById("unordered-list");
+
+  let newListItem = document.createElement("p");
+  newListItem.innerHTML = "test";
+
+  unorderedList.appendChild(newListItem);
+});
+*/
