6f7753b80dd578badaef37c3b2f9bbab415279cb
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2021-09-15 11:31:29 +0300

    First button working

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 /* placeholder
+ */
+
+console.log("Hello world!");
+
+/*
+if (document.readyState !== "loading") {
+  console.log("Document is ready");
+  intializeCode();
+} else {
+  document.addEventListener("DOM content loaded", function () {
+    console.log("Document ready after waiting!");
+    intializeCode();
+  });
+}
+
+function intializeCode() {
+*/
+
+/*
+addButton.addEventListener("click", function () {
+  /*
+    const textArea = document.getElementById("text-area");
+
+    let newText = document.createElement("p");
+    newText.innerHTML = "test";
+
+    textArea.appendChild(newText);
 
 
+
+  let list = document.getElementById("unordered-list");
+  let listItem = document.createElement("li");
+  let userComment = document.getElementById("text-area").value;
+  let textNode = document.createTextNode(userComment);
+  textNode.appendChild(listItem);
+  document.getElementById("unordered-list").appendChild(textNode);
 */
 
 const addButton = document.getElementById("add-comment");
 
-/*
 addButton.addEventListener("click", function () {
-  const unorderedList = document.getElementById("unordered-list");
+  const uList = document.getElementById("unordered-list");
 
-  let newListItem = document.createElement("p");
-  newListItem.innerHTML = "test";
+  let newListItem = document.createElement("li");
+  newListItem.innerHTML = "List item";
 
-  unorderedList.appendChild(newListItem);
+  uList.appendChild(newListItem);
 });
-*/
-
-addButton.addEventListener("click", function () {
-  const textArea = document.getElementById("text-area");
-
-  let newText = document.createElement("p");
-  newText.innerHTML = "test";
-
-  textArea.appendChild(newText);
-});
-
-/*
-const addButton = document.getElementById("add-comment");
-
-addButton.addEventListener("click", function () {
-  const unorderedList = document.getElementById("unordered-list");
-
-  let newListItem = document.createElement("p");
-  newListItem.innerHTML = "test";
-
-  unorderedList.appendChild(newListItem);
-});
-*/
